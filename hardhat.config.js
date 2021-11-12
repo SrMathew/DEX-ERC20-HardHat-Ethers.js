3fb5df1ff40e25c19e310191dbcbbb4b9eabb12f
MateoCornejo <mateoh.cornejo@gmail.com>
2021-11-12 15:48:42 -0300

    Unnecessary task eliminated

--- hardhat.config.js
+++ hardhat.config.js
@@ -7,10 +7,7 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
     console.log(account.address);
   }
 });
-// task action function receives the Hardhat Runtime Environment as second argument
-task("accountsweb3", "Prints accounts", async (_, { web3 }) => {
-  console.log(await web3.eth.getAccounts());
-});
+
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
